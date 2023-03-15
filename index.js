b0bc562781f8363dcb7551807b131e7ede344291
lewis-otwori <lewisotwori@gmail.com>
2023-03-15 08:48:10 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+
+}
+
+function mondayWork(activity = 'go to the office'){
+    return`This Monday, I will ${activity}.`
+}
+
+mondayWork("work from home");
+
+function wrapAdjective(){
+
+}
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
+  
+
